eadc1c4b71199331dc8f432cc63025294f5f82f1
Stephanos2911 <ioannoustephanos2002@gmail.com>
2022-10-11 15:21:39 +0200

    Add project files.

---
+++ Views/Shared/Insideproductmenu.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages.Forms
+{
+    public class InsideproductmenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+  //      public IActionResult Onpost()
+		//{
+  //          if (ModelState.IsValid == false)
+		//	{
+  //              return Page();
+		//	}
+
+  //          return RedirectToPage("/Index");
+		//}
+    }
+}
