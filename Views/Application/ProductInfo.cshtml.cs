c2f3098bbe2001a2ad79fcd52c45df5e4d25294a
MehmetAkif70 <1043550@hr.nl>
2022-11-30 10:43:27 +0100

    asd
    asd

---
+++ Views/Application/ProductInfo.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserApplication.Views.Application;
+
+public class ProductInfo : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
