1d54cff8c8ff71baecf352a302f0a260367910de
Stephanos2911 <ioannoustephanos2002@gmail.com>
2022-12-05 18:41:49 +0100

    Added Entity Framework injection

---
+++ Models/Store.cs
@@ -0,0 +1,12 @@
+﻿namespace UserApplication.Models
+{
+    public class Store
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+        public string SiteLink { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using UserApplication.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+// Add services to the container.
+var DatabaseConnectionString = builder.Configuration.GetConnectionString("ProjectCDBConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(DatabaseConnectionString));
+//options.UseMySql(DatabaseConnectionString, ServerVersion.AutoDetect(DatabaseConnectionString)));
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
