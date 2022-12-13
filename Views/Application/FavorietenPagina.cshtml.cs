1441b3ffdf10d88002ca8199bc23e3f496645253
MehmetAkif70 <1043550@hr.nl>
2022-12-13 11:29:54 +0100

    favorieten pagina

---
+++ Views/Application/FavorietenPagina.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserApplication.Views.Application;
+
+public class FavorietenPagina : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
