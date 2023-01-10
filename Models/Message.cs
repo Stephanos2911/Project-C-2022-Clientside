8a9f81971e9ef0a09ab79959f3692fe8148d5f5b
MehmetAkif70 <1043550@hr.nl>
2023-01-10 22:40:15 +0100

    Update Message.cs

--- Models/Message.cs
+++ Models/Message.cs
@@ -15,12 +15,11 @@ namespace UserApplication.Models
         [Required(ErrorMessage = "veld is verplicht")]
         [DataType(DataType.PhoneNumber)]
         [Phone]
-<<<<<<< HEAD
+
         public string PhoneNumber { get; set; }
 
         public bool IsAnswered { get; set; }
-=======
-        public int PhoneNumber { get; set; }
->>>>>>> parent of 3dca8f2 (nieuw startpagina, pop ups en hoovers)
+
+
     }
 }
