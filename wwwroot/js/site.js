cf7d7ad88c8d6a6c12424fa977d1cca8b95ee4f8
MehmetAkif70 <1043550@hr.nl>
2023-01-15 02:54:52 +0100

    favorite button is working correctly

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,19 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const favoriteButton = document.getElementById("favorite-button");
-const favoriteIndicator = document.getElementById("favorite-indicator");
 
-favoriteButton.addEventListener("click", function() {
-    if (localStorage.getItem("favorite")) {
-        localStorage.removeItem("favorite");
-        favoriteIndicator.innerHTML = "";
-    } else {
-        localStorage.setItem("favorite", true);
-        favoriteIndicator.innerHTML = "Favorited";
-    }
-});
-
-if (localStorage.getItem("favorite")) {
-    favoriteIndicator.innerHTML = "Favorited";
-}
